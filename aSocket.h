f625b5c3e5cffa61735cb3910e907bf8f8f77a64
Brendan Moran <brendan.moran@arm.com>
2014-12-03 14:21:38 +0000

    (Untested) UDP Asynch Sockets now build

--- aSocket.h
+++ aSocket.h
@@ -1,18 +1,20 @@
 #ifndef MBED_SOCKET_H
 #define MBED_SOCKET_H
 
+#include <mbed.h>
 #include <stddef.h>
 #include <stdint.h>
 #include "socket_types.h"
+#include "socket_types_impl.h"
 #include "CThunk.h"
 
 class aSocket {
 protected:
 
     aSocket(handler_t &defaultHandler) :
-            _defaultHandler(&defaultHandler)
+        _defaultHandler(defaultHandler), _irq(this)
     {
-        _irq.callback(&aSocket::_eventHandler);
+        _irq.callback(&aSocket::_nvEventHandler);
     }
     virtual ~aSocket() {}
 
@@ -21,7 +23,14 @@ protected:
 protected:
     handler_t _defaultHandler;
     CThunk<aSocket> _irq;
+
+public:
     struct socket _socket;
+
+private:
+    void _nvEventHandler(void * arg) {
+        _eventHandler(arg);
+    }
 };
 
 
