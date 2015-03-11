5c5251b64388d75aafddffe63eafa4491044dde7
Brendan Moran <brendan.moran@arm.com>
2015-03-11 14:50:47 +0000

    Add documentation to the primary classes

--- mbed-net-sockets/TCPStream.h
+++ mbed-net-sockets/TCPStream.h
@@ -4,32 +4,54 @@
  * PackageLicenseDeclared: Apache-2.0
  * Copyright 2015 ARM Holdings PLC
  */
- #include <stddef.h>
- #include <stdint.h>
- #include <Ticker.h>
- #include "socket_types.h"
- #include "TCPAsynch.h"
- #include "buffer.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <Ticker.h>
+#include "socket_types.h"
+#include "TCPAsynch.h"
+#include "buffer.h"
 
 class TCPStream: public TCPAsynch {
 public:
-  /* Socket Creation API */
-  TCPStream(const socket_stack_t stack);
-  ~TCPStream();
-
-public:
-  socket_error_t connect(const SocketAddr *address, const uint16_t port, const handler_t onConnect);
-  void onDisconnect(const handler_t h) {_onDisconnect = h;}
-
-
+    /**
+     * TCP socket constructor.
+     * Does not allocate an underlying TCP Socket instance.
+	 * @param[in] stack The network stack to use for this socket.
+     */
+	TCPStream(const socket_stack_t stack);
+	/**
+	 * TCP socket destructor
+	 */
+	~TCPStream();
+    /**
+     * Connect to a remote host.
+     * Initates the TCP Connection process.  onConnect is called when the connection
+     * is acknowledged.
+     * @param[in] address The remote host to connect to
+     * @param[in] port The remote port to connect to
+	 * @param[in] onConnect
+     * @return SOCKET_ERROR_NONE on success, or an error code on failure
+     */
+	socket_error_t connect(const SocketAddr *address, const uint16_t port,
+			const handler_t onConnect);
+	/**
+	 * Set a disconnect handler
+	 * This handler only needs to be configured once onConnect has been called
+	 * @param[in] h the handler to call when a connection is disconnected
+	 */
+	void onDisconnect(const handler_t h) { _onDisconnect = h; }
 
 protected:
-  void _eventHandler(struct socket_event *ev);
+	/**
+	 * Internal event handler
+	 * @param[in] ev the event to handle
+	 */
+	void _eventHandler(struct socket_event *ev);
 
 protected:
-  handler_t _onConnect;
-  handler_t _onDisconnect;
-  uint16_t _port;
+	handler_t _onConnect;
+	handler_t _onDisconnect;
+	uint16_t _port;
 };
 
 #endif // MBED_TCPStream_H
--- mbed-net-sockets/UDPaSocket.h
+++ mbed-net-sockets/UDPaSocket.h
@@ -13,31 +13,35 @@
 /* UDP socket class */
 class UDPaSocket: public aSocket {
 public:
-    /* Socket Creation API */
     /**
      * UDP socket constructor.
-     * Requires a port because the underlying UDP socket is allocated and bound at creation time. If an error occurs
-     * during construction, the defaultHandler will be called.
-     * @param[in] port The local port to listen on
-     * @param[in] defaultHandler The default handler to use if no transmissions have been initiated or on error
+     * Does not allocate an underlying UDP Socket instance.
+	 * @param[in] stack The network stack to use for this socket.
      */
     UDPaSocket(socket_stack_t stack);
-
+    /**
+     * UDP Socket destructor
+     */
+    ~UDPaSocket();
+    /**
+     * Open a UDP socket
+     * Instantiates and initializes the underlying socket. Receive is started immediately after
+     * the socket is opened.
+     * @param[in] af Address family (SOCKET_AF_INET4 or SOCKET_AF_INET6), currently only IPv4 is supported
+     * @return SOCKET_ERROR_NONE on success, or an error code on failure
+     */
     socket_error_t inline open(const socket_address_family_t af)
     {
     	return aSocket::open(af,SOCKET_DGRAM);
     }
-
     /**
-     * Specify the listening port and address for the (server) socket
-     * This is not necessary for a client socket
-     * @param[in] address The internet address to bind (may be 0.0.0.0/INET_ADDR_ANY)
-     * @param[in] port The port to listen on
-     * @return Returns 0 on success or an error code on failure
+     * Connect to a remote host.
+     * This is an internal configuration API only.  No network traffic is generated.
+     * @param[in] address The remote host to connect to
+     * @param[in] port The remote port to connect to
+     * @return SOCKET_ERROR_NONE on success, or an error code on failure
      */
     socket_error_t connect(const SocketAddr *address, const uint16_t port);
-    ~UDPaSocket();
-
 };
 
 #endif // MBED_UDPaSocket_H
