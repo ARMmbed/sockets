7a0abb0204548418ff1ab5ea90fc2db401fb6a42
Brendan Moran <brendan.moran@arm.com>
2015-05-28 10:13:48 +0100

    Fix TCP echo client test

--- test/echo-tcp-client/main.cpp
+++ test/echo-tcp-client/main.cpp
@@ -13,17 +13,19 @@ struct s_ip_address {
 };
 
 char out_buffer[] = "Hello World\n";
+char buffer[256];
 
 class TCPEchoClient {
 public:
-    TCPEchoClient(socket_stack_t stack, char * ext_buf) :
-        _stream(stack), buffer(ext_buf), done(false)
+    TCPEchoClient(socket_stack_t stack) :
+        _stream(stack), _done(false), _disconnected(true)
         {
         }
     socket_error_t start_test(char * host_addr, uint16_t port)
     {
         _port = port;
-        done = false;
+        _done = false;
+        _disconnected = true;
         return _stream.resolve(host_addr,handler_t(this, &TCPEchoClient::onDNS));
     }
     void onDNS(socket_error_t err)
@@ -38,6 +40,8 @@ public:
         TEST_EQ(err, SOCKET_ERROR_NONE);
         /* Register the read handler */
         _stream.setOnReadable(handler_t(this, &TCPEchoClient::onRx));
+        _stream.setOnSent(handler_t(this, &TCPEchoClient::onSent));
+        _stream.setOnDisconnect(handler_t(this, &TCPEchoClient::onDisconnect));
         /* Send the query packet to the remote host */
         err = _stream.connect(&_resolvedAddr, _port, handler_t(this,&TCPEchoClient::onConnect));
         if(!TEST_EQ(err, SOCKET_ERROR_NONE)) {
@@ -47,7 +51,10 @@ public:
     void onConnect(socket_error_t err)
     {
         TEST_EQ(err, SOCKET_ERROR_NONE);
+        _disconnected = false;
+        _unacked += sizeof(out_buffer) - 1;
         err = _stream.send(out_buffer, sizeof(out_buffer) - 1);
+
         TEST_EQ(err, SOCKET_ERROR_NONE);
     }
     void onRx(socket_error_t err)
@@ -58,32 +65,50 @@ public:
         buffer[n] = 0;
         char out_success[] = "{{success}}\n{{end}}\n";
         char out_failure[] = "{{failure}}\n{{end}}\n";
+        int rc;
         if (n > 0)
         {
             buffer[n] = '\0';
-            printf("%s", buffer);
-            if (strncmp(out_buffer, buffer, sizeof(out_buffer) - 1) == 0) {
+            printf("%s\r\n", buffer);
+            rc = strncmp(out_buffer, buffer, sizeof(out_buffer) - 1);
+            if (TEST_EQ(rc, 0)) {
+                _unacked += sizeof(out_success) - 1;
                 err = _stream.send(out_success, sizeof(out_success) - 1);
-                done = true;
+                _done = true;
                 TEST_EQ(err, SOCKET_ERROR_NONE);
             }
         }
-        if (!done) {
+        if (!_done) {
+            _unacked += sizeof(out_failure) - 1;
             err = _stream.send(out_failure, sizeof(out_failure) - 1);
-            done = true;
+            _done = true;
             TEST_EQ(err, SOCKET_ERROR_NONE);
         }
-        _stream.close();
     }
-    bool isdone() {
-        return done;
+    void onSent(socket_error_t err)
+    {
+        (void)err;
+        socket_event_t *event = _stream.getEvent();
+        _unacked -= event->i.t.sentbytes;
+        if (_done && (_unacked == 0)) {
+            _stream.close();
+        }
+    }
+    void onDisconnect(socket_error_t err)
+    {
+        (void) err;
+        _disconnected = true;
+    }
+    bool isDone() {
+        return _done && (_unacked == 0) && _disconnected;
     }
 protected:
     TCPStream _stream;
     SocketAddr _resolvedAddr;
     uint16_t _port;
-    char * buffer;
-    volatile bool done;
+    volatile bool _done;
+    volatile bool _disconnected;
+    volatile size_t _unacked;
 };
 
 
@@ -113,13 +138,13 @@ int main() {
 
     printf("TCPClient IP Address is %s" NL, eth.getIPAddress());
     sprintf(buffer, "%d.%d.%d.%d", ip_addr.ip_1, ip_addr.ip_2, ip_addr.ip_3, ip_addr.ip_4);
-    client = new TCPEchoClient(SOCKET_STACK_LWIP_IPV4,buffer);
+    client = new TCPEchoClient(SOCKET_STACK_LWIP_IPV4);
 
     err = client->start_test(buffer,port);
     if (!TEST_EQ(err, SOCKET_ERROR_NONE)) {
         printf("TCPCllient unable to connect to %s:%d" NL, buffer, port);
     }
-    while (!client->isdone()) __WFI();
+    while (!client->isDone()) __WFI();
 
     delete client;
     eth.disconnect();
